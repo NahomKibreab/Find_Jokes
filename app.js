71085abc994fddd6c3b249503b5067136c0d7d91
NahomKibreab <nknahom@gmail.com>
2021-01-12 00:15:16 -0700

    fetching json added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const btnJoke = document.querySelector('#btn_jokes');
 const displayJoke = document.querySelector('.card-text');
 
-btnJoke.addEventListener('click', function () {
-  displayJoke.innerText = 'New Jokes';
+const getJoke = async () => {
+  const config = { headers: { Accept: 'application/json' } };
+  const req = await axios.get('https://icanhazdadjoke.com/', config);
+  return req.data.joke;
+};
+
+btnJoke.addEventListener('click', async function () {
+  displayJoke.innerText = `"${await getJoke()}"`;
 });
